3c818260d170c2ca3e2b4a932a91ec184edee189
René Muhl <rmuhl@mailbox.org>
2016-06-12 18:25:50 +0200

    Added calculation of left linear and removed manual xml walking

--- FCA/Benchmark.hs
+++ FCA/Benchmark.hs
@@ -4,83 +4,66 @@ import FCA.Helpers
 import Import hiding (get)
 
 import Control.Monad
--- import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 import Data.Maybe
 import qualified Data.ByteString.Lazy as BSL
 import qualified Codec.Archive.Zip as Zip
-import qualified Text.XML.Cursor as Cursor
 import Presenter.StarExec.Space
 import Presenter.DOI
 import Presenter.PersistHelper
 import System.FilePath
 import Presenter.StarExec.Commands
 import Data.List (isSuffixOf, stripPrefix)
-import Data.Text (pack, isInfixOf)
+import Data.Text (pack)
 import TPDB.Input.Memory
 import TPDB.Data
 import TPDB.Data.Attributes
 import Data.Either
--- module to load benchmark information from latest tpdb archive into the database
 
 -- perhabs is that the source of strictness:
+insertLatestBenchmarks :: HandlerT App IO ()
 insertLatestBenchmarks = insertBenchmarkInfoFromTPDB "TPDB-4d76dd84fd49.zip"
 
-existsBenchmarkInDB :: Int -> Handler Bool
-existsBenchmarkInDB bid = do
+-- check database if benchmark  with given id exists
+doesNotExistsBenchmarkInDB :: Int -> Handler Bool
+doesNotExistsBenchmarkInDB bid = do
   record <- getPersistBenchmarkInfo $ StarExecBenchmarkID bid
   case record of
     Nothing -> return True
     Just _  -> return False
 
-createBenchmarkInfo :: Int -> Text -> Text -> Int -> BenchmarkInfo
-createBenchmarkInfo bid name type' numberOfRules = BenchmarkInfo bid name type' numberOfRules defaultDate
+-- create benchmarkinfo instances from given data
+createBenchmarkInfo :: Int -> Text -> Text -> Int -> Bool -> BenchmarkInfo
+createBenchmarkInfo bid bName type' numberOfRules leftLinear = BenchmarkInfo bid bName type' numberOfRules leftLinear defaultDate
 
-
-getNumberOfRules :: Cursor.Cursor -> Int
-getNumberOfRules cursor = length $ Cursor.child cursor >>= Cursor.element "trs" >>= Cursor.child
-                         >>= Cursor.element "rules" >>= Cursor.child >>= Cursor.element "rule"
-
-getNumberOfVariables :: Cursor.Cursor -> Int
-getNumberOfVariables cursor = length $ Cursor.child cursor >>= Cursor.element "trs" >>= Cursor.child
-                        >>= Cursor.element "var" -- >>= Cursor.child >>= Cursor.element "rule"
-
-
+-- insert benchmark instances from tpdb zip
+-- including new attributes for formal concept analysis: number of rules and left linear
 insertBenchmarkInfoFromTPDB :: FilePath -> HandlerT App IO ()
 insertBenchmarkInfoFromTPDB fPath = do
   Just sp <- liftIO $ getDefaultSpaceXML "TPDB-10.3_XML.zip"
-
-  let mm = invertMap $ spaceToNames sp
+  let benchmarkPathBidRel = invertMap $ spaceToNames sp
   bs <- liftIO $ BSL.readFile fPath
+  let xmlEntries = fmap (\e -> do
+                               let path = fromJust $ stripPrefix "TPDB-4d76dd84fd49/" $ Zip.eRelativePath e
+                               let content = Zip.fromEntry e
+                               let benchmarkId = fromJust $ M.lookup (pack path) benchmarkPathBidRel
+                               (takeFileName path, path, content, benchmarkId)
+                             ) $
+                             filter (\ e -> isSuffixOf ".xml" $ Zip.eRelativePath e) $ Zip.zEntries $ Zip.toArchive bs
 
-  let xmlEntries = take 5 $ fmap (\e -> do
-                             let path = fromJust $ stripPrefix "TPDB-4d76dd84fd49/" $ Zip.eRelativePath e
-                             let content = Zip.fromEntry e
-                             (takeFileName path, path, content)
-                            ) $
-                            filter (\ e -> isSuffixOf ".xml" $ Zip.eRelativePath e) $ Zip.zEntries $ Zip.toArchive bs
+  nonExistingBenchmarks <- filterM (\(_, _, _, StarExecBenchmarkID bid) -> doesNotExistsBenchmarkInDB $ bid) xmlEntries
 
-
-  -- FIXME: add filtering: calculate for benchmarks that aren't in the database yet
   -- FIXME: add doi handling
-
-  tpdbInstances <- mapM liftIO $ map (\(fn, _, content) -> get fn content) xmlEntries
+  tpdbInstances <- mapM liftIO $ map (\(fn, _, content, _) -> get fn content) nonExistingBenchmarks
 
   -- only for trs instances atm
-  let trsRules = fmap (rules) $ lefts $ rights tpdbInstances
+  let trsRules = fmap (rules) . lefts $ rights tpdbInstances
   let numberOfRulesAttrs = fmap length trsRules
   let leftLinearAttrs = fmap (left_linear . compute_attributes) trsRules
-  let benchmarkInfos = fmap
-                  (\e -> do
-                    let path = fromJust $ stripPrefix "TPDB-4d76dd84fd49/" $ Zip.eRelativePath e
-                    let content = Zip.fromEntry e
-                    let benchmarkId = fromJust $ M.lookup (pack path) mm
-                    let cursor = cursorFromDOM content
-                    (pack $ takeFileName path, path, benchmarkId, getNumberOfRules cursor, getNumberOfVariables cursor)) $
-                  filter (\ e -> isSuffixOf ".xml" $ Zip.eRelativePath e) $ Zip.zEntries $ Zip.toArchive bs
-  -- remove this restriction later
-  -- let srsEntries = filter (\(_,path,_,_,_) -> "SRS_Relative" `isInfixOf` (pack path)) benchmarkInfos
-  let benchmarkInstances = fmap (\(name,_ ,StarExecBenchmarkID bid ,numberOfRules ,_ ) -> createBenchmarkInfo bid name "" numberOfRules) $ take 1  benchmarkInfos
+  let benchmarkData = zipWith3 (\(t1,t2,t3,t4) r l -> (t1,t2,t3,t4,r,l)) nonExistingBenchmarks numberOfRulesAttrs leftLinearAttrs
 
-  nonExistingBenchmarks <- filterM (\b -> existsBenchmarkInDB $ benchmarkInfoStarExecId b) benchmarkInstances
-  runDB $ insertMany_ nonExistingBenchmarks
+  -- FIXME: remove use of take
+  let benchmarkInstances = fmap
+                          (\(fn, _, _ , StarExecBenchmarkID bid, numberOfRules, leftLinear)  -> createBenchmarkInfo bid (pack fn) "" numberOfRules leftLinear) $
+                          take 8000 benchmarkData
+  runDB $ insertMany_ benchmarkInstances
